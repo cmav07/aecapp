44a334e3bb947f58cad81ae82cabf85f4a53f401
cmav07 <151847701+cmav07@users.noreply.github.com>
2025-01-02 07:52:01 +0530

    Update MainActivity.java

--- calc/java/com/example/simple_calculator/MainActivity.java
+++ calc/java/com/example/simple_calculator/MainActivity.java
@@ -1,5 +1,6 @@
-package com.example.simple_calculator;
+package com.example.calculator;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -13,91 +14,86 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
-    Button b1,b2,b3,b4,bp,bmi,bmu,beq;
-    TextView et;
-    Float v1,v2,v3;
-    Boolean add,mu,min;
+
+
+    Button b1, b2, bplus, beql;
+    EditText ET_Result;
+    Float v1, v2;
+    Boolean add;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
+//        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        b1 = (Button)findViewById(R.id.b1);
-        b2 = (Button)findViewById(R.id.b2);
-        b3 = (Button)findViewById(R.id.b3);
-        b4 = (Button)findViewById(R.id.b4);
-        bp = (Button)findViewById(R.id.bp);
-        bmu = (Button)findViewById(R.id.bmu);
-        bmi = (Button)findViewById(R.id.bmi);
-        beq = (Button)findViewById(R.id.beq);
-        et = (EditText) findViewById(R.id.res);
+
+
+        b1 = (Button)findViewById(R.id.btn_one);
+        b2 = (Button)findViewById(R.id.btn_two);
+
+        bplus = (Button)findViewById(R.id.btn_plus);
+        beql = (Button)findViewById((R.id.btn_eql));
+        TextView ET_Result = (TextView) findViewById(R.id.ET_Result);
+
+
         b1.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
-            public void onClick(View v) {
-                et.setText(et.getText()+"7");
+            public void onClick(View view) {
+                ET_Result.setText(ET_Result.getText() + "1");
             }
         });
+
+
         b2.setOnClickListener(new View.OnClickListener() {
+            @SuppressLint("SetTextI18n")
             @Override
-            public void onClick(View v) {
-                et.setText(et.getText()+"8");
+            public void onClick(View view) {
+                ET_Result.setText(ET_Result.getText() + "2");
             }
         });
-        b3.setOnClickListener(new View.OnClickListener() {
+
+        bplus.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View v) {
-                et.setText(et.getText()+"4");
-            }
-        });
-        b4.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                et.setText(et.getText()+"1");
-            }
-        });
-        bp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v1 = Float.parseFloat(et.getText()+"");
-                add = true;
-                et.setText(null);
-            }
-        });
-        bmi.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v1 = Float.parseFloat(et.getText()+"");
-                min = true;
-                et.setText(null);
-            }
-        });
-        bmu.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v1 = Float.parseFloat(et.getText()+"");
-                mu = true;
-                et.setText(null);
-            }
-        });
-        beq.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                v2 = Float.parseFloat(String.valueOf(et.getText()));
-                if(add){
-                    et.setText(v1+v2+"");
-                    add = false;
-                }
-                else if(min){
-                    et.setText(v1-v2+"");
-                    min = false;
-                }
-                else if(mu){
-                    et.setText(v1*v2+"");
-                    mu = false;
+            public void onClick(View view) {
+                String input = ET_Result.getText().toString().trim();
+                if (!input.isEmpty()) {
+                    try {
+                        v1 = Float.parseFloat(input);
+                        add = true;
+                        ET_Result.setText(null);
+                    } catch (NumberFormatException e) {
+                        ET_Result.setError("Invalid input");
+                    }
+                } else {
+                    ET_Result.setError("Please enter a number");
                 }
             }
         });
+
+
+        beql.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String input = ET_Result.getText().toString().trim();
+                if (!input.isEmpty()) {
+                    try {
+                        v2 = Float.parseFloat(input);
+                        if (add) {
+                            ET_Result.setText(String.valueOf(v1 + v2));
+                            add = false;
+                        }
+                    } catch (NumberFormatException e) {
+                        ET_Result.setError("Invalid input");
+                    }
+                } else {
+                    ET_Result.setError("Please enter a number");
+                }
+            }
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
