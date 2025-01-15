98bf8da9634e071f9591f41c2a9de01543c9f275
cmav07 <151847701+cmav07@users.noreply.github.com>
2025-01-16 00:34:57 +0530

    Update MainActivity.java

--- wallpaper/java/com/example/wallpaper/MainActivity.java
+++ wallpaper/java/com/example/wallpaper/MainActivity.java
@@ -1,50 +1,70 @@
-package com.example.wallpaper;
+package com.example.myapplication;
 
+import android.annotation.SuppressLint;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Bundle;
 import android.app.WallpaperManager;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.os.Bundle;
-import android.view.View;
+import android.graphics.drawable.Drawable;
+import android.os.Handler;
 import android.widget.Button;
 import android.widget.Toast;
-
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-//import androidx.core.graphics.BitmapCompat;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+import androidx.core.content.ContextCompat;
 
 import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
-    Button wallcg;
+    Button changewallpaper;
+    WallpaperManager wpm;
+    int id = 1;
+    Handler handler = new Handler(); // Handler to post to UI thread
+
+    @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        wallcg = (Button) findViewById(R.id.btn_wp);
-        wallcg.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                changewp();
-            }
-        });
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        wpm = WallpaperManager.getInstance(this);
+        changewallpaper = findViewById(R.id.btn_click);
+        changewallpaper.setOnClickListener(view -> setWallpaper());
     }
-    private void changewp(){
-        Bitmap bitmap = BitmapFactory.decodeResource(getResources(),R.drawable.img);
-        WallpaperManager manager = WallpaperManager.getInstance(getApplicationContext());
-        try{
-            manager.setBitmap(bitmap);
-            Toast.makeText(this,"Wallpaper set Succefully",Toast.LENGTH_SHORT).show();
-        }catch (IOException e){
-            throw new RuntimeException(e);
-        }
+
+    private void setWallpaper() {
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                // Set wallpaper based on id
+                Drawable drawable = null;
+                if (id == 1) {
+                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.one);
+                    id = 2;
+                } else if (id == 2) {
+                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.two);
+                    id = 3;
+                } else if (id == 3) {
+                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.three);
+                    id = 4;
+                } else if (id == 4) {
+                    drawable = ContextCompat.getDrawable(MainActivity.this, R.drawable.four);
+                    id = 1;
+                }
+
+                if (drawable != null) {
+                    Bitmap wallpaper = ((BitmapDrawable) drawable).getBitmap();
+                    try {
+                        wpm.setBitmap(wallpaper);
+                        Toast.makeText(MainActivity.this, "Wallpaper Changed", Toast.LENGTH_SHORT).show();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                        Toast.makeText(MainActivity.this, "Error setting wallpaper", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        }, 0);  // Immediately run the task
     }
 }
+chal gaya bhai
+wallpaper ka
